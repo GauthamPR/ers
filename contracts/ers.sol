477a1c53cd35478259168f628c64e5223b9bc9b2
swath-i <swathi.rejitha@gmail.com>
2022-04-06 01:10:07 +0530

    variables

--- contracts/ers.sol
+++ contracts/ers.sol
@@ -1,4 +1,18 @@
 pragma solidity ^0.8.13;
 contract ers {
     uint public taskCount = 0;
+
+    struct Task {
+    uint student_id_hash;
+    uint answer_sheet_hash;
+    uint public_addr_uploader;
+    string content;
+    bool completed;
+  }
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+}
 }
