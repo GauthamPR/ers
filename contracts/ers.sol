66e9ef366e1123a238ffc0e0e107c5cafeb6b6c0
swath-i <swathi.rejitha@gmail.com>
2022-04-06 21:39:42 +0530

    ch

--- contracts/ers.sol
+++ contracts/ers.sol
@@ -1,16 +1,17 @@
 pragma solidity ^0.8.13;
 contract ers {
 
-    struct Task {
+    struct Code {
+    uint exam_id;
     string student_id_hash;
     string answer_sheet_hash;
     address public_addr_uploader;
     
   }
-    mapping(string => Task) public tasks;
+    mapping(string => Code) public codes;
 
-    function createTask(string memory answer_sheet_hash) public {
-    taskCount ++;
-    tasks[taskCount] = Task(student_id_hash,answer_sheet_hash,public_addr_uploader);
+    function createCode(uint string string address memory exam_id answer_sheet_hash answer_sheet_hash public_addr_uploader) 
+    public {
+    codes[answer_sheet_hash] = Code(exam_id,student_id_hash,answer_sheet_hash,public_addr_uploader);
 }
 }
